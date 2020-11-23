a1dad4a73c5d43cc0d4f178f30c89b48e446129c
Charlie <13856015+KyrneDev@users.noreply.github.com>
2020-11-23 12:18:16 -0800

    Update index.js

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -2,28 +2,23 @@ import app from 'flarum/app';
 
 app.initializers.add('flarum-flags', () => {
   app.extensionData.for('flarum-flags')
-    .registerSettings([
-        {
-          setting: 'flarum-flags.guidelines_url',
-          type: 'text',
-          label: app.translator.trans('flarum-flags.admin.settings.guidelines_url_label')
-        },
-        {
-          setting: 'flarum-flags.can_flag_own',
-          type: 'boolean',
-          label: app.translator.trans('flarum-flags.admin.settings.flag_own_posts_label')
-        }
-      ]
-    )
-    .registerPermission(
-      {
+    .registerSetting({
+      setting: 'flarum-flags.guidelines_url',
+      type: 'text',
+      label: app.translator.trans('flarum-flags.admin.settings.guidelines_url_label')
+    }, 15)
+    .registerSetting({
+      setting: 'flarum-flags.can_flag_own',
+      type: 'boolean',
+      label: app.translator.trans('flarum-flags.admin.settings.flag_own_posts_label')
+    })
+    .registerPermission({
         icon: 'fas fa-flag',
         label: app.translator.trans('flarum-flags.admin.permissions.view_flags_label'),
         permission: 'discussion.viewFlags'
       }, 'moderate', 65)
 
-    .registerPermission(
-      {
+    .registerPermission({
         icon: 'fas fa-flag',
         label: app.translator.trans('flarum-flags.admin.permissions.flag_posts_label'),
         permission: 'discussion.flagPosts'
