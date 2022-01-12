94dfd4771076fee38e7d689ae69b01049cb1702d
Ian Morland <ian@morland.me>
2022-01-12 15:38:00 +0000

    Remove old event

--- src/Command/DeleteFlagsHandler.php
+++ src/Command/DeleteFlagsHandler.php
@@ -49,9 +49,6 @@ class DeleteFlagsHandler
 
         $actor->assertCan('viewFlags', $post->discussion);
 
-        // remove beta 17
-        $this->events->dispatch(new FlagsWillBeDeleted($post, $actor, $command->data));
-
         foreach ($post->flags as $flag) {
             $this->events->dispatch(new Deleting($flag, $actor, $command->data));
         }
